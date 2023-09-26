e7efef91cdf913d6a764739c2d4cfe99e11f34d6
Rhuan Emanuel <rhnrod@gmail.com>
2023-09-25 23:25:58 -0300

    initial commit

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}.`;
+}
+console.info(saudacao('Rhuan'));
+console.info(multiplica(12, 5));
---
+++ src/exercicio.ts
@@ -0,0 +1,10 @@
+function multiplica(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string):string {
+    return `Olá ${nome}.`
+}
+
+console.info(saudacao('Rhuan'));
+console.info(multiplica(12, 5));
